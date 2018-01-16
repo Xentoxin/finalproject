9ac70c8b7752c5b6e2bce8a807cf6abcdd8d345d
Xentoxin <hchen15@stuy.edu>
2018-01-15 22:55:52 -0500

    newrecord(int i){}

--- hospital management system.c
+++ hospital management system.c
@@ -34,4 +34,46 @@ void edit1();
 void editrecord();
 void switch1();
 
+void main(){}
+void newrecord(int l)
+{
+	char q;
+	p.sn=l;
+	displaydepartment();
+	gotoxy(5,14);textcolor(10);
+	cprintf("Record of patient no:");
+	printf(" %d",l);
+	gotoxy(5,17);
+	cprintf("Name:");
 
+	gotoxy(5,20);
+	cprintf("Address:");
+
+	gotoxy(5,23);
+	cprintf("Age: ");
+
+	gotoxy(5,26);
+	cprintf("Sex(m/f): ");
+
+	gotoxy(5,29);
+	cprintf("Disease Descrp:");
+	gotoxy(9,30);
+	cprintf("(In Short)");
+
+	gotoxy(5,33);
+	cprintf("Reff. Specialist no:");
+	fflush(stdin);gotoxy(10,17);
+	scanf("%[^\n]",p.name);
+	p.name[0]=toupper(p.name[0]);
+	gotoxy(14,20);
+	fflush(stdin);
+	scanf("%[^\n]",p.address);
+	gotoxy(10,23);
+	fflush(stdin);
+	scanf("%d",&p.age);
+	gotoxy(15,26);
+	fflush(stdin);
+	scanf("%c",&p.ch);
+	fflush(stdin);gotoxy(22,29);
+	scanf("%[^\n]",p.disease);
+}
